ca9da883ff87f4c9b32ffb13c661b21650706f16
Spencer LaValle <slavalle@Spencers-MacBook-Air.local>
2021-08-23 20:28:00 -0600

    Re-ordered helper_functions

--- test/helper_functions.py
+++ test/helper_functions.py
@@ -1,5 +1,18 @@
 import requests, base64, random, string, json
 from threading import Thread
+class ThreadWithReturnValue(Thread):
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs={}, Verbose=None):
+        Thread.__init__(self, group, target, name, args, kwargs)
+        self._return = None
+    def run(self):
+        # print(type(self._target))
+        if self._target is not None:
+            self._return = self._target(*self._args,
+                                                **self._kwargs)
+    def join(self, *args):
+        Thread.join(self, *args)
+        return self._return
 
 def build_base_url(host = "http://localhost", port=8000):
     return host + ":" + str(port) + ""
@@ -40,16 +53,3 @@ def decode_base64_hash(base64_hash :str):
     decoded_bytes = base64.b64decode(base64_hash)
     decoded_str = str(decoded_bytes, "utf-8")
     return decoded_str
-class ThreadWithReturnValue(Thread):
-    def __init__(self, group=None, target=None, name=None,
-                 args=(), kwargs={}, Verbose=None):
-        Thread.__init__(self, group, target, name, args, kwargs)
-        self._return = None
-    def run(self):
-        # print(type(self._target))
-        if self._target is not None:
-            self._return = self._target(*self._args,
-                                                **self._kwargs)
-    def join(self, *args):
-        Thread.join(self, *args)
-        return self._return
