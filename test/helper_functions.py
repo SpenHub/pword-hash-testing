63139911b1f42b0dae7484e3080aa94be6b51065
Spencer LaValle <slavalle@Spencers-MacBook-Air.local>
2021-08-23 18:44:05 -0600

    Staying DRY

--- test/helper_functions.py
+++ test/helper_functions.py
@@ -36,3 +36,16 @@ def decode_base64_hash(base64_hash :str):
     decoded_bytes = base64.b64decode(base64_hash)
     decoded_str = str(decoded_bytes, "utf-8")
     return decoded_str
+class ThreadWithReturnValue(Thread):
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs={}, Verbose=None):
+        Thread.__init__(self, group, target, name, args, kwargs)
+        self._return = None
+    def run(self):
+        # print(type(self._target))
+        if self._target is not None:
+            self._return = self._target(*self._args,
+                                                **self._kwargs)
+    def join(self, *args):
+        Thread.join(self, *args)
+        return self._return
--- test/test_data_validity.py
+++ test/test_data_validity.py
@@ -3,20 +3,6 @@ import hashlib as hl
 import base64
 from threading import Thread
 
-class ThreadWithReturnValue(Thread):
-    def __init__(self, group=None, target=None, name=None,
-                 args=(), kwargs={}, Verbose=None):
-        Thread.__init__(self, group, target, name, args, kwargs)
-        self._return = None
-    def run(self):
-        # print(type(self._target))
-        if self._target is not None:
-            self._return = self._target(*self._args,
-                                                **self._kwargs)
-    def join(self, *args):
-        Thread.join(self, *args)
-        return self._return
-
 def test_sha512():
     # Compute SHA512, base64 encode, and compare
     # with "hash/<job_id>" response
@@ -43,7 +29,7 @@ def test_stats_num_jobs():
     base64_hashes = []
 
     for i in range(NUM_THREADS):
-        t = ThreadWithReturnValue(target=hf.api_post_random_password, args=())
+        t = hf.ThreadWithReturnValue(target=hf.api_post_random_password, args=())
         t.daemon = True
         threads.append(t)
 
